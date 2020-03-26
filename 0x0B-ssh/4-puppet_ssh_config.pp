a20c2dcb5be9875d79a5570e8785d36ec68d2934
Michelle Giraldo <michellegsld@gmail.com>
2020-03-26 10:11:09 -0400

    [Update] Fix puppet-lint

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # Task 4: Client configuration file (w/ Puppet) 
 include stdlib
-file_line { 'turn off passwd auth':
-  ensure  =>  present,
-  path    =>  '/etc/ssh/ssh_config',
-  line    =>  'PasswordAuthentication no',
+file_line {
+  'Turn off passwd auth':
+  ensure =>  present,
+  path   =>  '/etc/ssh/ssh_config',
+  line   =>  'PasswordAuthentication no',
 ;
-'declare identity file':
-  ensure  =>  present,
-  path    =>  '/etc/ssh/ssh_config'
-  line    =>  'IdentifyFile ~/.ssh/holberton'
+'Declare identity file':
+  ensure =>  present,
+  path   =>  '/etc/ssh/ssh_config'
+  line   =>  'IdentifyFile ~/.ssh/holberton'
 }
