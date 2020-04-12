26a85aa4bf9b9d9feb2e4130f87a033d018eec48
Michelle Giraldo <michellegsld@gmail.com>
2020-04-11 22:37:50 -0400

    [Update] Go back to sed and specify a line

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 # Exact copy of task 0 but done as puppet, each line of task 0 is within command
 exec {'command':
-    command => "sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo \"add_header X-Served-By: $HOSTNAME;\" >> /etc/nginx/sites-available/default && sudo service nginx start",
+    command => "sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i \"42i \ add_header X-Served-By: $HOSTNAME;\" >> /etc/nginx/sites-available/default && sudo service nginx start",
     provider => shell,
 }
