6feda71adb6665995ad96630577b476328adc59e
Michelle Giraldo <michellegsld@gmail.com>
2020-05-04 17:05:06 -0400

    [Initial] Making two get requests in order to find name info and then information about tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Task 0:
+0-gather_data_from_an_API.py
+
+Returns information for each given employee ID,
+the TODO list progress.
+Uses a specific API.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    req = requests.get(url)
+    name = req.json()["name"]
+    req = requests.get(url + '/todos', params={"completed": "true"})
+    amount = len(req.json())
+    print("Employee {} is done with tasks ({}/20)".format(name, amount))
+    for i in range(amount):
+        print("{}".format(req.json()[i]["title"]))
