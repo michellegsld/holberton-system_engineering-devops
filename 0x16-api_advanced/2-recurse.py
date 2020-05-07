44bb6b195dde22c1101a34fcdc982feeb7fea3d8
Michelle Giraldo <michellegsld@gmail.com>
2020-05-07 19:22:17 -0400

    [Update] Change what the range end was to length of [data][children]

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -20,7 +20,7 @@ def recurse(subreddit, hot_list=[], param={}):
     req = requests.get(url, params=param, headers=head, allow_redirects=False)
 
     try:
-        for child in range(0, req.json()['data']['dist']):
+        for child in range(0, len(req.json()['data']['children'])):
             post = req.json()['data']['children'][child]['data']['title']
             hot_list.append(post)
         param['after'] = req.json()['data']['after']
