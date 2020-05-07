ce1aba64af4eb0f80e01e3fcbcf8879f1f8e8cc8
Michelle Giraldo <michellegsld@gmail.com>
2020-05-07 14:51:43 -0400

    [Initial] Use header that sets user-agent and make allow_redirects False in order to get subcount from subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Task 0:
+Queries the Reddit API and returns total number of subscribers,
+if a subreddit is invalid then it returns 0
+0-subs.py
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns:
+    Number of subscribers to a subreddit
+    or 0 on invalid subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    head = {'user-agent': 'michellegsld-holberton'}
+    req = requests.get(url, headers=head, allow_redirects=False)
+    try:
+        return req.json()['data']['subscribers']
+    except:
+        return 0
