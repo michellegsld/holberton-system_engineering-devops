d264fb1818d9709b601bc4265b878c934e5319ff
Michelle Giraldo <michellegsld@gmail.com>
2020-05-07 17:30:39 -0400

    [Initial] Add parameter called child to def in order to know what whild to call

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Task 2:
+Queries the Reddit API and returns a list containing
+titles of all hot articles for a given subreddit
+If a subreddit is invalid then it returns None
+2-recurse.py
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], child=0):
+    """
+    Returns:
+    List of all hot titles in a subreddit
+    Or None is the subreddit is invalid
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    head = {'user-agent': 'michellegsld-holberton'}
+    req = requests.get(url, headers=head, allow_redirects=False)
+
+    try:
+        hot_list.append(req.json()['data']['children'][child]['data']['title'])
+    except:
+        return None
+
+    recurse(subreddit, hot_list, child=child+1)
+    return hot_list
